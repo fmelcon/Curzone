af3b5edff2b9eae7d2b0defb60cadf7672cedfdf
Franco Melcon <fmelcon@live.com>
2021-04-06 17:53:03 -0300

    add Hero animation and some styles

--- js/main.js
+++ js/main.js
@@ -44,15 +44,75 @@ function cargarEventListeners() {
   });
 }
 
-jQuery(".herotext").hide(() => {
-  jQuery(".herotext").fadeIn(8000, () => {
-    jQuery(".herotext").fadeOut(10000);
-  });
-});
+jQuery(".herotext").hide();
+// jQuery(".herotext").hide(() => {
+//   jQuery(".herotext").fadeIn(8000, () => {
+//     jQuery(".herotext").fadeOut(10000);
+//   });
+// });
 
 // =============================================================================
 // Funciones
 // =============================================================================
+
+consoleText(
+  [
+    "Welcome Aboard",
+    "Learn code with us",
+    "Best courses online",
+    "What do you waiting for?",
+    "HTML CSS JavaScript And More...",
+  ],
+  "text",
+  ["#ffc927"]
+);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ["#fff"];
+  var visible = true;
+  var con = document.getElementById("console");
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id);
+  target.setAttribute("style", "color:" + colors[0]);
+  window.setInterval(function () {
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(function () {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute("style", "color:" + colors[0]);
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function () {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 500);
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount);
+      letterCount += x;
+    }
+  }, 100);
+  window.setInterval(function () {
+    if (visible === true) {
+      con.className = "console-underscore hidden";
+      visible = false;
+    } else {
+      con.className = "console-underscore";
+
+      visible = true;
+    }
+  }, 400);
+}
 
 function addName(e) {
   e.preventDefault();
