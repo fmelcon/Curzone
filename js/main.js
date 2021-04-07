4e90b72c727f0ef7845a276899f0b8f8c6fac73e
Franco Melcon <fmelcon@live.com>
2021-04-07 20:56:23 -0300

    change color of subtitles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // =============================================================================
 // Variables globales
 // =============================================================================
-
 const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const listaCursos = document.querySelector("#lista-cursos");
